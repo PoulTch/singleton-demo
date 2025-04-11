9ffa18035e77e34b61b13b0a8255685340a5be43
PoulTch <pavelchar@gmail.com>
2025-04-11 12:21:18 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 class Logger
+  # 'a' - mean add to end of text
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  # class method
   def self.say_something
     puts "haha"
+  end
+
+  # instance method
+  # 'a' - mean add to end of text
+  def log_something wat
+    @f.puts wat
   end
 end
 
 Logger.say_something
+
+logger = Logger.new
+logger.log_something 'hey!'
