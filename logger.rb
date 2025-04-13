4ec1b08f543ce9aab8f72986d5671010f7f573c2
PoulTch <pavelchar@gmail.com>
2025-04-13 12:41:48 +0300

    Refactoring : extract logger to separate file

---
+++ logger.rb
@@ -0,0 +1,19 @@
+class Logger
+  # 'a' - mean add to end of text
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  @@x = Logger.new
+
+  def self.instance
+    return @@x
+  end
+
+  # instance method
+  def log_something wat
+    @f.puts wat
+  end
+
+    private_class_method :new
+end
